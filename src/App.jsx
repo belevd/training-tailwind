15a18e441d922501e5ef47ef73d912d9965fc43b
belevd <bankir35@mail.ru>
2021-04-23 09:53:14 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Header } from './components/Header';
+import { PopularDestinations } from './components/PopularDestinations';
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <PopularDestinations />
+    </>
+  )
+}
